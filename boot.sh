2503b2a76b20a3df996c96cf949586e2b6a67a12
MudrakshnMcShaw <contact.mudraksh@gmail.com>
2026-03-21 12:21:07 +0530

    bug fix

--- boot.sh
+++ boot.sh
@@ -21,3 +21,6 @@ fi
 cd "/root/Executor_Error/"
 $PM2 delete Exec_Error
 $PM2 start 'exec_error.py' --interpreter='/root/Executor_Error/venv/bin/python3' --namespace='Exec_Error' --name='Exec_Error_1' --time -- Exec_Error Exec_Error_1
+$PM2 start 'exec_error.py' --interpreter='/root/Executor_Error/venv/bin/python3' --namespace='Exec_Error' --name='Exec_Error_2' --time -- Exec_Error Exec_Error_2
+$PM2 start 'exec_error.py' --interpreter='/root/Executor_Error/venv/bin/python3' --namespace='Exec_Error' --name='Exec_Error_3' --time -- Exec_Error Exec_Error_3
+$PM2 start 'exec_error.py' --interpreter='/root/Executor_Error/venv/bin/python3' --namespace='Exec_Error' --name='Exec_Error_4' --time -- Exec_Error Exec_Error_4
